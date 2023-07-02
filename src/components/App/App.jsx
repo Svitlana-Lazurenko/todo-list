1c93ebbf0b8f8f9ba6829b2b1bf3848bb19c1a7c
Svitlana-Lazurenko <svitsvit222@gmail.com>
2023-07-02 19:59:20 +0300

    task

---
+++ src/components/App/App.jsx
@@ -0,0 +1,69 @@
+import { nanoid } from 'nanoid';
+import { useState, useEffect } from 'react';
+
+import {
+  Container,
+  Grid,
+  GridItem,
+  Header,
+  SearchForm,
+  Section,
+  Text,
+  Todo,
+} from 'components';
+
+export const App = () => {
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem('todos')) ?? []
+  );
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = text => {
+    const todo = {
+      id: nanoid(),
+      text,
+    };
+
+    setTodos([...todos, todo]);
+  };
+
+  const handleSubmit = data => {
+    addTodo(data);
+  };
+
+  const deleteTodo = id => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  return (
+    <>
+      <Header />
+      <Section>
+        <Container>
+          <SearchForm onSubmit={handleSubmit} />
+
+          {todos.length === 0 && (
+            <Text textAlign="center">There are no any todos ... </Text>
+          )}
+
+          <Grid>
+            {todos.length > 0 &&
+              todos.map((todo, index) => (
+                <GridItem key={todo.id}>
+                  <Todo
+                    id={todo.id}
+                    text={todo.text}
+                    counter={index + 1}
+                    onClick={deleteTodo}
+                  />
+                </GridItem>
+              ))}
+          </Grid>
+        </Container>
+      </Section>
+    </>
+  );
+};
